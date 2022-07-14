c5310c02ad631d6d651167b5139a1fb7c59e1dab
dingyifan <dingyifan@apowo.com>
2022-07-14 17:57:05 +0800

    init

---
+++ Source/MyTrial/MyTrialGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyTrialGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMyTrialGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMyTrialGameMode();
+};
+
+
+
